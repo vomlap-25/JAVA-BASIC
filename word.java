e353257abdfa15b4f326c2ba9008ad9775f59a63
vomlap-25 <261592928+vomlap-25@users.noreply.github.com>
2026-08-24 12:54:26 +0530

    ....

---
+++ word.java
@@ -0,0 +1,10 @@
+public class word {
+    public static void main(String[] args) {
+        String s = "hello";
+
+for (int i = s.length() - 1; i >= 0; i--) {
+    System.out.println(s.charAt(i));
+}
+    }
+    
+}
